386d918bb275694417731c97919b3fe09c9ebd7e
Michael Hindle <michael.hindle@opendeusto.es>
2024-05-02 23:32:39 +0200

    solucionando el actualizar

--- appDeustomachGestionTickets/models.py
+++ appDeustomachGestionTickets/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from appDeustomachGestionEmpleados.models import Empleado
+from django.utils import timezone
 
 
 class Ticket(models.Model):
@@ -13,11 +14,19 @@ class Ticket(models.Model):
     estado_ticket = models.CharField(max_length=50)
     empleado_asignado = models.ForeignKey(Empleado, on_delete=models.CASCADE)
     comentarios = models.TextField(null=True, blank=True)
-    created = models.DateTimeField(auto_now_add=True, null=True, blank=True)
-    updated = models.DateTimeField(auto_now=True, null=True, blank=True)
+    created = models.DateTimeField(default=timezone.now)
+    updated = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            # Si es un nuevo objeto, establece el campo created
+            self.created = timezone.now()
+        self.updated = timezone.now()  # Siempre actualiza el campo updated
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.numero_referencia
+
 
 
 class Meta:
